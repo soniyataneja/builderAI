fb23b7acae73726a1950058018b50c8bffc2a410
Soniya Taneja <soniyataneja23@gmail.com>
2026-09-07 16:27:00 +0530

    added user authentication

---
+++ server/routes/authRoutes.js
@@ -0,0 +1,14 @@
+import { Router } from "express";
+import { login, logout, me, register } from "../controllers/authControllers.js";
+import { authMiddleware } from "../middleware/authMiddleware";
+
+const authRouter = Router()
+
+authRouter.post('/register', register)
+authRouter.post('/login', login)
+authRouter.post('/logout', logout)
+authRouter.get('/me', authMiddleware,me)
+
+export default authRouter
+
+
