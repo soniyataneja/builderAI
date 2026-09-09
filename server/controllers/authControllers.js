aad15934c7b890233a90a057a86bcb9954aebe17
Soniya Taneja <soniyataneja23@gmail.com>
2026-09-09 23:53:41 +0530

    fix production authentication cookies

--- server/controllers/authControllers.js
+++ server/controllers/authControllers.js
@@ -9,7 +9,7 @@ const setSessionCookie = (res, payload)=>{
     res.cookie('token', token, {
         httpOnly: true,
         secure: process.env.NODE_ENV === "production",
-        sameSite: "lax",
+        sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
         maxAge: 30 * 24 * 60 * 60 * 1000, // 30 days
         path: "/",
     })
